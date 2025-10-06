9d4586e85333ba556ca2711c4aed3590a1aa5be9
vehbi6 <budurivehbi@gmail.com>
2025-10-06 11:25:44 +0200

    react

--- dita7_json/src/components/Bloglist.jsx
+++ dita7_json/src/components/Bloglist.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Viewlist from './Viewlist';
 
 
  function Bloglist(props) {
--- dita7_json/src/components/Home.jsx
+++ dita7_json/src/components/Home.jsx
@@ -1,34 +1,42 @@
-import React,{useEffect, useState} from 'react'
-import Bloglist from './Bloglist.jsx'
-import Viewlist from './Viewlist.jsx'
+import React, { useEffect, useState } from 'react';
+import Bloglist from './Bloglist.jsx';
 
- function Home() {
+function Home() {
+  const [list, setList] = useState([]);
+  const [error, setError] = useState(null); // 1. Add error state
 
-const [list, setList] = useState([
-  {id:1, emri:"vehbi",age:25, city:"prishtine"},
-  {id:2, emri:"arianit",age:30, city:"gjilan"},
-  {id:3, emri:"fatlind",age:28, city:"ferizaj"},
-  {id:4, emri:"bujar",age:35, city:"prizren"},
-  {id:5, emri:"bekim",age:22, city:"peje"},
-])
+  useEffect(() => {
+    fetch('http://localhost:1234/listqd')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Failed to fetch data'); // 1. Throw error if response is not ok
+        }
+        return res.json();
+      })
+      .then(data => {
+        setList(data);
+        setError(null); // clear error if fetch succeeds
+      })
+      .catch(err => {
+        setError(err.message); // 2. Set error message
+      });
+  }, []);
 
-useEffect(()=>{
-  console.log("Component loaded");
-  console.log(list);
-},[])
-
-const deleteButton=(id)=>{
-  const newList=list.filter(list=>list.id!==id)
-  setList(newList);
-}
+  const deleteButton = (id) => {
+    const newList = list.filter(item => item.id !== id);
+    setList(newList);
+  };
 
   return (
     <>
+      <h1>Home page</h1>
 
-    <h1>Home page</h1>
-  <Bloglist list={list} x="Lista e Nxenesve" deleteButton={deleteButton} viewButton={viewButton}/>
-         
+      {/* 3. Show error if it exists */}
+      {error && <div style={{ color: 'red' }}>Error: {error}</div>}
+
+      <Bloglist list={list} x="Lista e Nxenesve" deleteButton={deleteButton} />
     </>
-  )
+  );
 }
-export default Home
+
+export default Home;
